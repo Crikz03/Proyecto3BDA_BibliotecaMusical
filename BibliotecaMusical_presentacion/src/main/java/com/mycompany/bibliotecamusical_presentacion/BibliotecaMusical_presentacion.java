414688bbf0e862d2b75195c85f4886999c643d12
CriCrin03 <chris012203@gmail.com>
2024-11-22 02:47:51 -0700

    creacion de los proyectos de java

---
+++ BibliotecaMusical_presentacion/src/main/java/com/mycompany/bibliotecamusical_presentacion/BibliotecaMusical_presentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibliotecamusical_presentacion;
+
+/**
+ *
+ * @author Chris
+ */
+public class BibliotecaMusical_presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
