d5205e94703529aa4270f95619db4b38d69eaf7c
PaulinaRayos <163494315+PaulinaRayos@users.noreply.github.com>
2024-11-27 12:17:16 -0700

    Insercion 15 artistas

--- BibliotecaMusical_presentacion/src/main/java/newpackage/InsercionMasivaArtistas.java
+++ BibliotecaMusical_presentacion/src/main/java/newpackage/InsercionMasivaArtistas.java
@@ -16,6 +16,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import objetos.Artistas;
 import recursos.GestorImagenesMongo;
 import recursos.Imagen;
@@ -39,7 +41,7 @@ public class InsercionMasivaArtistas {
             documento.append("nombre", artista.getNombre());
             documento.append("tipo", artista.getTipo());
             documento.append("genero", artista.getGenero());
-            
+
             // Procesar la imagen
             Imagen imagen = artista.getImagen();
             if (imagen != null) {
@@ -62,38 +64,65 @@ public class InsercionMasivaArtistas {
         MongoClient cliente = new MongoClient("localhost", 27017); // Conexión a MongoDB
         MongoDatabase baseDeDatos = cliente.getDatabase("BibliotecaMusical");
         MongoCollection<Document> coleccion = baseDeDatos.getCollection("artistas");
-        
+
         // Insertar los documentos
         coleccion.insertMany(documentos);
-        
+
         System.out.println("Inserción masiva completada");
     }
 
     // Método para preparar una lista de artistas con sus imágenes (como ejemplo)
     private static List<Artistas> obtenerArtistasConImagenes() {
         List<Artistas> artistas = new ArrayList<>();
+
+        /*File imagen1 = new File("C:/Users/pauli/OneDrive/Documentos/GitHub/Proyecto3BDA_BibliotecaMusical/BibliotecaMusical_presentacion/images/yomigod.png");
+        Imagen imagen1Obj = GestorImagenesMongo.crearImagen("yomigod.png", imagen1);
         
-        // Aquí deberías crear tus objetos de artistas. Ejemplo:
-        try {
-            File imagen1 = new File("C:/Users/Chris/Documents/GitHub/Proyecto3BDA_BibliotecaMusical/BibliotecaMusical_presentacion/images/yomigod.png");
-            Imagen imagen1Obj = GestorImagenesMongo.crearImagen("yomigod.png", imagen1);
-            
-            Artistas artista1 = new Artistas();
-            artista1.setNombre("Artista 3");
-            artista1.setTipo("Tipo 1");
-            artista1.setGenero("Género 1");
-            artista1.setImagen(imagen1Obj);
-
-            // Agregar el artista a la lista
-            artistas.add(artista1);
-
-            // Agregar más artistas aquí según sea necesario...
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Artistas artista1 = new Artistas();
+        artista1.setNombre("Artista 3");
+        artista1.setTipo("Tipo 1");
+        artista1.setGenero("Género 1");
+        artista1.setImagen(imagen1Obj);
         
+        // Agregar el artista a la lista
+        artistas.add(artista1);*/
+        artistas.add(crearArtista("Bad Bunny", "Solista", "Reggaeton", "badbunny.png", "./images/badbunny.jpg"));
+        artistas.add(crearArtista("Dua Lipa", "Solista", "Pop", "dualipa.png", "./images/dualipa.jpg"));
+        artistas.add(crearArtista("Taylor Swift", "Solista", "Pop", "taylorswift.png", "./images/taylorswift.jpg"));
+        artistas.add(crearArtista("The Weeknd", "Solista", "R&B", "theweeknd.png", "./images/theweekend.jpg"));
+        artistas.add(crearArtista("Billie Eilish", "Solista", "Alternative Pop", "billieeilish.png", "./images/billieeilish.jpg"));
+        artistas.add(crearArtista("Harry Styles", "Solista", "Pop Rock", "harrystyles.png", "./images/harrystyles.jpg"));
+        artistas.add(crearArtista("Ed Sheeran", "Solista", "Pop", "edsheeran.png", "./images/edsheeran.jpg"));
+        artistas.add(crearArtista("Adele", "Solista", "Soul", "adele.png", "./images/adele.jpg"));
+        artistas.add(crearArtista("Rosalía", "Solista", "Flamenco Pop", "rosalia.png", "./images/rosalia.jpg"));
+        artistas.add(crearArtista("Shakira", "Solista", "Pop Latino", "shakira.png", "./images/shakira.png"));
+        artistas.add(crearArtista("Karol G", "Solista", "Reggaeton", "karolg.png", "./images/karolg.jpg"));
+        artistas.add(crearArtista("Olivia Rodrigo", "Solista", "Pop Rock", "oliviarodrigo.png", "./images/oliviarodrigo.jpg"));
+        artistas.add(crearArtista("Bruno Mars", "Solista", "Funk", "brunomars.png", "./images/brunomars.jpg"));
+        artistas.add(crearArtista("Ariana Grande", "Solista", "Pop", "arianagrande.png", "./images/arianagrande.png"));
+        artistas.add(crearArtista("J Balvin", "Solista", "Reggaeton", "jbalvin.png", "./images/jbalvin.jpg"));
+
+        // Imprimir información de los artistas creados
+        artistas.forEach(artista -> System.out.println("Artista: " + artista.getNombre() + " | Tipo: " + artista.getTipo() + " | Género: " + artista.getGenero()));
+
         return artistas;
     }
-}
 
+    private static Artistas crearArtista(String nombre, String tipo, String genero, String nombreImagen, String rutaImagen) {
+        try {
+            File imagenFile = new File(rutaImagen);
+            Imagen imagenObj = GestorImagenesMongo.crearImagen(nombreImagen, imagenFile);
+
+            Artistas artista = new Artistas();
+            artista.setNombre(nombre);
+            artista.setTipo(tipo);
+            artista.setGenero(genero);
+            artista.setImagen(imagenObj);
+
+            return artista;
+        } catch (IOException ex) {
+            Logger.getLogger(InsercionMasivaArtistas.class.getName()).log(Level.SEVERE, "Error al crear artista: " + nombre, ex);
+            return null;
+        }
+    }
+}
